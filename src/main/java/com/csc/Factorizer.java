85f372a12b94181ed4a01c8f14c3ff6c256e51e3
vs25965 <vs25965@mail.harpercollege.edu>
2024-08-30 22:23:19 -0500

    New changes made

--- src/main/java/com/csc/Factorizer.java
+++ src/main/java/com/csc/Factorizer.java
@@ -1,14 +1,60 @@
 package com.csc;
+import java.util.Scanner;
+import java.util.ArrayList;
+
 
 public class Factorizer {
 
-  // You should implement your solution here.
-  // Feel free to delete this example method when you implement your solution.
-  public int exampleMethod(int someArgument) {
-    if (someArgument > 5) {
-      return 1;
-    } else {
-      return 2;
+public static void main (String[] args)
+{
+  
+   Scanner in = new Scanner(System.in);
+   System.out.println("Enter a number: ");
+   int numberin = in.nextInt();
+
+   ArrayList primenumbs = new ArrayList();
+   
+   if (numberin == 1 || numberin == 0 )
+   {
+     System.out.println("null");
+   }
+   else 
+   {
+    for (int i = 2; i < numberin; i++)
+     {
+      if (numberin % i == 0 & i == 2 ) 
+      {
+        primenumbs.add(i); 
+      }
+      else if (numberin % i == 0 & i%2 != 0)
+      {
+        double s = Math.sqrt(i);
+        double n = s*s;
+
+        if (i != n )
+        {
+          primenumbs.add(i);
+        }
+          
+      }
+      
+     }
+
+     System.out.println("This is the prime factors of " + numberin + ": " + primenumbs);
+    }  
+
     }
-  }
-}
+
+ }
+
+  
+
+
+
+
+
+
+
+
+ 
+
