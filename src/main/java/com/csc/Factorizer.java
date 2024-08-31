7918560c84a9240850849d429da2eb4b50854c3b
vs25965 <vs25965@mail.harpercollege.edu>
2024-08-30 22:25:27 -0500

    new changes made

--- src/main/java/com/csc/Factorizer.java
+++ src/main/java/com/csc/Factorizer.java
@@ -5,49 +5,42 @@ import java.util.ArrayList;
 
 public class Factorizer {
 
-public static void main (String[] args)
+public static primefactors(int numberin)
 {
-  
-   Scanner in = new Scanner(System.in);
-   System.out.println("Enter a number: ");
-   int numberin = in.nextInt();
-
-   ArrayList primenumbs = new ArrayList();
+  ArrayList primenumbs = new ArrayList();
    
-   if (numberin == 1 || numberin == 0 )
-   {
-     System.out.println("null");
-   }
-   else 
-   {
-    for (int i = 2; i < numberin; i++)
+  if (numberin == 1 || numberin == 0 )
+  {
+    System.out.println("null");
+  }
+  else 
+  {
+   for (int i = 2; i < numberin; i++)
+    {
+     if (numberin % i == 0 & i == 2 ) 
      {
-      if (numberin % i == 0 & i == 2 ) 
-      {
-        primenumbs.add(i); 
-      }
-      else if (numberin % i == 0 & i%2 != 0)
-      {
-        double s = Math.sqrt(i);
-        double n = s*s;
-
-        if (i != n )
-        {
-          primenumbs.add(i);
-        }
-          
-      }
-      
+       primenumbs.add(i); 
      }
+     else if (numberin % i == 0 & i%2 != 0)
+     {
+       double s = Math.sqrt(i);
+       double n = s*s;
 
-     System.out.println("This is the prime factors of " + numberin + ": " + primenumbs);
-    }  
-
+       if (i != n )
+       {
+         primenumbs.add(i);
+       }
+         
+     }
+     
     }
 
- }
+    System.out.println("This is the prime factors of " + numberin + ": " + primenumbs);
+   }  
 
-  
+}
+
+}
 
 
 
