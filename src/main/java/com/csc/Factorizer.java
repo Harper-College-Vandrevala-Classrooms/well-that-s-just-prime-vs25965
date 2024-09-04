2e7908da94999262a004d517bc445e72a0a056da
vs25965 <vs25965@mail.harpercollege.edu>
2024-09-04 15:13:50 -0500

    Fix issues from blackboard feedback
    Hello professor. I saw your feedback from blackboard. I fix the problems, and the program should work now.

--- src/main/java/com/csc/Factorizer.java
+++ src/main/java/com/csc/Factorizer.java
@@ -2,45 +2,55 @@ package com.csc;
 import java.util.Scanner;
 import java.util.ArrayList;
 
-
 public class Factorizer {
 
-public static primefactors(int numberin)
-{
-  ArrayList primenumbs = new ArrayList();
-   
-  if (numberin == 1 || numberin == 0 )
+  public static void main (String[] args)
   {
-    System.out.println("null");
+     System.out.println("Enter a number to check its prime factors: ");
+     Scanner in = new Scanner(System.in);
+     int numberin = in.nextInt();
+     primefactors(numberin);
   }
-  else 
-  {
-   for (int i = 2; i < numberin; i++)
+
+    public static void primefactors(int numberin)
     {
-     if (numberin % i == 0 & i == 2 ) 
-     {
-       primenumbs.add(i); 
-     }
-     else if (numberin % i == 0 & i%2 != 0)
-     {
-       double s = Math.sqrt(i);
-       double n = s*s;
-
-       if (i != n )
-       {
-         primenumbs.add(i);
-       }
-         
-     }
+      ArrayList primenumbs = new ArrayList();
      
-    }
+       
+      if (numberin == 1 || numberin == 0 )
+      {
+       System.out.println("null");
+      }
+      else 
+      {
+       for (int i = 2; i < numberin; i++)
+        {
+         if (numberin % i == 0 & i == 2 ) 
+         {
+           primenumbs.add(i); 
+         }
+         else if (numberin % i == 0 & i%2 != 0)
+         {
+           double s = Math.sqrt(i);
+           double n = s*s;
+    
+           if (i != n )
+           {
+             primenumbs.add(i);
+           }
+             
+         }
+         
+        }
+    
+        System.out.println("This is the prime factors of " + numberin + ": " + primenumbs);
+       }  
+    
+  }
+    
+  }
 
-    System.out.println("This is the prime factors of " + numberin + ": " + primenumbs);
-   }  
 
-}
-
-}
 
 
 
